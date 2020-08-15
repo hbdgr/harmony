9828b2207ace6177aa893aa58e9530c296fe73aa
hbdgr <hbdgr@protonmail.com>
2020-08-15 13:12:56 +0200

    add missing route

---
+++ src/views/Missing.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="missing-page">
+    404
+  </div>
+</template>
+
+<script>
+
+export default {}
+</script>
+
+<style lang="scss">
+  @import '../styles/App.scss';
+</style>
